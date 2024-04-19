c4cbf21281f81c2bc2c34879ddc116b8152401f4
Valeria Rubio Herrera <valeriarubioh@gmail.com>
2024-04-19 17:52:25 -0500

    first commit

---
+++ lib/data.ts
@@ -0,0 +1,45 @@
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+
+export const skillsData = [
+  "Java",
+  "SpringBoot",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git & GitHub",
+  "HTML",
+  "CSS",
+  "MySQL",
+  "MongoDB",
+  "Postman",
+  "Python",
+] as const;
